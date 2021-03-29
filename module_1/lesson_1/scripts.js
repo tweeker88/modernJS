b68ca0abef67849af2137fc254f4b0f9d6e0cb37
tweeker88 <maxim.teplov97@gmail.com>
2021-03-29 14:40:29 +0300

    module_1: типы данных

---
+++ module_1/lesson_1/scripts.js
@@ -0,0 +1,37 @@
+// number
+var number = 12;
+console.log(`Number: ${number}`);
+
+// bigInt
+var bigInt = 1234567890123456789012345678901234567890n;
+console.log(`bigInt: ${bigInt}`);
+
+// string
+var stringOne = 'First string';
+var stringTwo = "Second string";
+var stringThree = `Third string`;
+console.log(stringOne, stringTwo, stringThree);
+
+// boolean
+var checked = true;
+console.log(checked);
+
+// null
+var nullable = null;
+console.log(nullable);
+
+// undefined
+var undef;
+console.log(undef);
+
+// symbol
+var s1 = Symbol('id');
+var s2 = Symbol('id');
+console.log(s1, s2);
+
+// object
+var newObject = {
+    firstProperty: 'FirstValue',
+    secondProperty: 'SecondValue'
+};
+console.log(newObject);
